70eba61814ab7aeee9bb3469b49f2d32fcd85feb
Sarah McLain <sarahmclain0602@gmail.com>
2023-05-15 17:20:02 -0700

    sql homework challenge

--- EmployeeSQL/sql_homework.sql
+++ EmployeeSQL/sql_homework.sql
@@ -1,3 +1,60 @@
+CREATE TABLE departments (
+    dept_no VARCHAR   NOT NULL,
+    dept_name VARCHAR   NOT NULL,
+    CONSTRAINT pk_departments PRIMARY KEY (
+        dept_no
+     )
+);
+
+CREATE TABLE titles (
+    title_id VARCHAR   NOT NULL,
+    title VARCHAR   NOT NULL,
+    CONSTRAINT "pk_titles" PRIMARY KEY (
+        "title_id"
+     )
+);
+
+CREATE TABLE employees (
+    emp_no int   NOT NULL,
+    emp_title_id VARCHAR   NOT NULL REFERENCES titles.title_id,
+    birth_date DATE   NOT NULL,
+    first_name VARCHAR   NOT NULL,
+    last_name VARCHAR   NOT NULL,
+    sex VARCHAR   NOT NULL,
+    hire_date DATE   NOT NULL,
+    CONSTRAINT pk_employees PRIMARY KEY (
+        emp_no
+     )
+);
+
+CREATE TABLE dept_emp (
+    emp_no int   NOT NULL REFERENCES employees.emp_no,
+    dept_no VARCHAR   NOT NULL REFERENCES departments.dept_no,
+    CONSTRAINT pk_dept_emp PRIMARY KEY (
+        emp_no,dept_no
+     ) 
+);
+
+CREATE TABLE dept_manager (
+    dept_no VARCHAR   NOT NULL REFERENCES departments.dept_no,
+	emp_no int   NOT NULL REFERENCES employees.emp_no,
+    CONSTRAINT pk_dept_manager PRIMARY KEY (
+        dept_no,emp_no
+     )
+);
+
+
+CREATE TABLE salaries (
+    emp_no int   NOT NULL REFERENCES employees.emp_no,
+    salary int   NOT NULL,
+    CONSTRAINT pk_salaries PRIMARY KEY (
+        emp_no
+     )
+);
+
+
+
+
 SELECT e.emp_no, e.last_name, e.first_name, e.sex, s.salary
 FROM employees e
 	left join salaries s
